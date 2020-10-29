0738ac237bce73c88f68779359e4a355e3af2c54
cde_1001 <446226001@qq.com>
2020-10-29 13:57:58 +0800

    架构探险-第二阶段-1.0.1

--- src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -1,18 +1,72 @@
 package org.smart4j.chapter2.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.model.Customer;
+import org.smart4j.chapter2.util.PropsUtil;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class CustomerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+    private static final String  DRIVER;
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+
+    static {
+        Properties properties = PropsUtil.loadProps("config.properties");
+        DRIVER = properties.getProperty("jdbc.driver");
+        URL = properties.getProperty("jdbc.url");
+        USERNAME = properties.getProperty("jdbc.username");
+        PASSWORD = properties.getProperty("jdbc.password");
+        try {
+            Class.forName(DRIVER);
+        }catch (ClassNotFoundException e){
+            LOGGER.error("can not load jdbc driver",e);
+        }
+    }
 
     /**
      * 获取客户列表
      */
     public List<Customer> getCustomerList(String keyword){
-        //TODO
-        return null;
+        Connection connection = null;
+        List<Customer> customers = null;
+        try {
+            customers = new ArrayList<Customer>();
+            String sql = "select * from customer";
+            connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                Customer customer = new Customer();
+                customer.setId(resultSet.getLong("id"));
+                customer.setName(resultSet.getString("name"));
+                customer.setContact(resultSet.getString("contact"));
+                customer.setEmail(resultSet.getString("email"));
+                customer.setRemark(resultSet.getString("remark"));
+                customer.setTelephone(resultSet.getString("telephone"));
+                customers.add(customer);
+            }
+        }catch (SQLException e){
+            LOGGER.error(" execute sql failure ",e);
+        }finally {
+            if(connection != null){
+                try {
+                    connection.close();
+                }catch (SQLException e){
+                    LOGGER.error(" close connection failure ",e);
+                }
+            }
+        }
+        return customers;
     }
 
     /**
